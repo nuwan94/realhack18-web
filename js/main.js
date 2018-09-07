d390965c5df5473dc29861066e8a3c7e656eab28
nuwan94 <nuwansalawatta@gmail.com>
2018-09-08 00:06:23 +0530

    particlejsnew

--- js/main.js
+++ js/main.js
@@ -74,10 +74,10 @@
                     i.removeClass("open")
                 }
             }),
-            $(".dropdown-button").dropdown({
-                belowOrigin: !0,
-                constrainWidth: !1
-            }),
+            // $(".dropdown-button").dropdown({
+            //     belowOrigin: !0,
+            //     constrainWidth: !1
+            // }),
             setInterval(function () {
                 $(".carousel").carousel("next")
             }, 1e4),
@@ -123,106 +123,56 @@
 
 particlesJS("particles-js", {
     particles: {
-        number: {
-            value: 80,
-            density: {
-                enable: true,
-                value_area: 800
-            }
-        },
-        color: {
-            value: "#333"
-        },
-        shape: {
-            type: "circle",
-            stroke: {
-                width: 0,
-                color: "#333"
-            },
-            polygon: {
-                nb_sides: 5
-            }
-        },
-        opacity: {
-            value: 0.5,
-            random: false,
-            anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false
-            }
-        },
-        size: {
-            value: 3,
-            random: true,
-            anim: {
-                enable: false,
-                speed: 40,
-                size_min: 0.1,
-                sync: false
-            }
-        },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#333",
-            opacity: 0.4,
-            width: 1
-        },
-        move: {
-            enable: true,
-            speed: 6,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
-            attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200
-            }
-        }
+      number: { value: 50, density: { enable: true, value_area: 800 } },
+      color: { value: "#aa5dab" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#aa5dab" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#333",
+        opacity: .5,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: true,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 }
+      }
     },
     interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: {
-                enable: true,
-                mode: "grab"
-            },
-            onclick: {
-                enable: true,
-                mode: "push"
-            },
-            resize: true
-        },
-        modes: {
-            grab: {
-                distance: 400,
-                line_linked: {
-                    opacity: 1
-                }
-            },
-            bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            },
-            push: {
-                particles_nb: 4
-            },
-            remove: {
-                particles_nb: 2
-            }
-        }
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: false, mode: "grab" },
+        onclick: { enable: false, mode: "remove" },
+        resize: true
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { color: "#333"}},
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 10 },
+        remove: { particles_nb: 2 }
+      }
     },
     retina_detect: true
-});
+  });
