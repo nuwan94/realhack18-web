ba7d933ff53709a8b79aea20baaaaaa9edf1f9e4
nuwan94 <nuwansalawatta@gmail.com>
2018-09-08 15:43:57 +0530

    clock

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 ! function ($) {
     "use strict";
-
     var e = $(window);
     e.on("load", function () {
         $(".pre-loader").fadeOut("slow").delay(5000),
@@ -123,56 +122,179 @@
 
 particlesJS("particles-js", {
     particles: {
-      number: { value: 50, density: { enable: true, value_area: 800 } },
-      color: { value: "#aa5dab" },
-      shape: {
-        type: "circle",
-        stroke: { width: 0, color: "#aa5dab" },
-        polygon: { nb_sides: 5 },
-        image: { src: "img/github.svg", width: 100, height: 100 }
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
-      },
-      size: {
-        value: 5,
-        random: true,
-        anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#333",
-        opacity: .5,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 3,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        bounce: true,
-        attract: { enable: false, rotateX: 600, rotateY: 1200 }
-      }
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#aa5dab"
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#aa5dab"
+            },
+            polygon: {
+                nb_sides: 5
+            },
+            image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100
+            }
+        },
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#333",
+            opacity: .5,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: true,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+            }
+        }
     },
     interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: true, mode: "grab" },
-        onclick: { enable: false, mode: "remove" },
-        resize: true
-      },
-      modes: {
-        grab: { distance: 400, line_linked: { color: "#333"}},
-        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-        repulse: { distance: 200, duration: 0.4 },
-        push: { particles_nb: 10 },
-        remove: { particles_nb: 2 }
-      }
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "grab"
+            },
+            onclick: {
+                enable: false,
+                mode: "remove"
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    color: "#333"
+                }
+            },
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 10
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
     },
     retina_detect: true
-  });
+});
+
+/* JQuery.Countdown Clock */
+
+$(window).on('load', function () {
+    var labels = ['Days', 'Hours', 'Minutes', 'Seconds'],
+        eventDate = '2018/10/15 ',
+        template = _.template($('#clock-template').html()),
+        currDate = '00:00:00:00',
+        nextDate = '00:00:00:00',
+        parser = /([0-9]{2})/gi,
+        $example = $('#count-clock');
+
+    // Parse countdown string to an object
+    function strfobj(str) {
+        var parsed = str.match(parser),
+            obj = {};
+        labels.forEach(function (label, i) {
+            obj[label] = parsed[i]
+        });
+        return obj;
+    }
+    // Return the time components that diffs
+    function diff(obj1, obj2) {
+        var diff = [];
+        labels.forEach(function (key) {
+            if (obj1[key] !== obj2[key]) {
+                diff.push(key);
+            }
+        });
+        return diff;
+    }
+    // Build the layout
+    var initData = strfobj(currDate);
+    labels.forEach(function (label, i) {
+        $example.append(template({
+            curr: initData[label],
+            next: initData[label],
+            label: label
+        }));
+    });
+    // Starts the countdown
+    $example.countdown(eventDate, function (event) {
+        var newDate = event.strftime('%D:%H:%M:%S'),
+            data;
+        if (newDate !== nextDate) {
+            currDate = nextDate;
+            nextDate = newDate;
+            // Setup the data
+            data = {
+                'curr': strfobj(currDate),
+                'next': strfobj(nextDate)
+            };
+            // Apply the new values to each node that changed
+            diff(data.curr, data.next).forEach(function (label) {
+                var selector = '.%s'.replace(/%s/, label),
+                    $node = $example.find(selector);
+                // Update the node
+                $node.removeClass('flip');
+                $node.find('.curr').text(data.curr[label]);
+                $node.find('.next').text(data.next[label]);
+                // Wait for a repaint to then flip
+                _.delay(function ($node) {
+                    $node.addClass('flip');
+                }, 50, $node);
+            });
+        }
+    });
+});
